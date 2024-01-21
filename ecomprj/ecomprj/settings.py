77249d381e6962e48dfa1da59ab2d332288c4c11
Muchiri-cmd <davismuchiri21@gmail.com>
2024-01-21 05:42:40 +0300

    Setup custom django-admin page

--- ecomprj/ecomprj/settings.py
+++ ecomprj/ecomprj/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -134,3 +135,11 @@ MEDIA_ROOT=os.path.join(BASE_DIR,"media")    #All images sit
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+JAZZMIN_SETTINGS={
+    #'site_header':"",
+    #'site_brand':"",
+    #'site_logo':"",add site logo urlpath
+    'copyright':"Proudly By Davis",
+}
